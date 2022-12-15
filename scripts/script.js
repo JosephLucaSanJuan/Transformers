70a96eeb3890eb552fee56c94129f52e523a77f8
Joseph Luca San Juan Thompson <91873591+JosephLucaSanJuan@users.noreply.github.com>
2022-12-15 23:58:55 +0100

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+/**************************************************/
+/********** JAVASCRIPT - TRANFORMERS IDW **********/
+/**************************************************/
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
